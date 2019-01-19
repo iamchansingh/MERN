1c768101af35842e435a85b70f850d6121dac222
chanpreet <cp.chanpreet@gmail.com>
2019-01-19 17:55:04 +0530

    devtool setup failing on mobile final fix

--- client/src/store.js
+++ client/src/store.js
@@ -5,16 +5,28 @@ import rootReducer from "./reducers";
 const initialState = {};
 
 const middleware = [thunk];
+let store;
 const devTools =
   process.env.NODE_ENV !== "production"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
     : null;
+if (devTools) {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      devTools
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  applyMiddleware(...middleware)
-);
+    applyMiddleware(...middleware)
+  );
+}
 
 export default store;
